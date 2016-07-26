89fa8ef2a1d3aa0aef7bba1f199982a5ffdf7af1
ChristianUbillus <christianubi4521@gmail.com>
2016-07-25 22:28:11 -0500

    20160725 - DETAILS, EDITS, DELETE  - POPPUP

---
+++ WebDeveloper.Model/DTO/AddressModelView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDeveloper.Model.DTO
+{
+    public class AddressModelView
+    {
+        public String AddressLine1 { get; set; }
+        public String City { get; set; }
+        public String PostalCode { get; set; }
+    }
+}
